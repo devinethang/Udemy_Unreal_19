dc4ab62cae35169cc16a8203c602d1fe688bd870
Devin Graupmann <devinethang@gmail.com>
2019-09-01 13:16:59 -0700

    Print adding to variables

--- Udemy_Unreal_19.cpp
+++ Udemy_Unreal_19.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	int b = 2;
+	int c = 1;
+	cout << "The value of b is " << b << "." << endl;
+	cout << "The value of c is " << c << "." << endl;
+	cout << "The value of b + c is " << b+c << "." << endl;
     return 0;
 }
 
